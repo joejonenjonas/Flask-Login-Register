83d878933d39ef7fa41541afa0bbae7c77dafc3f
joejonenjonas <jonas.draps@student.odisee.be>
2022-01-25 15:04:50 +0000

    Update

--- runapp.sh
+++ runapp.sh
@@ -9,7 +9,7 @@ echo "COPY ./backend/app.py /home/myapp/" >> Dockerfile
 #open poort 8082
 echo "EXPOSE 8082" >> Dockerfile
 #installeren van requirements
-echo "COPY ./requirements.txt /home/myapp/requirements.txt" >> Dockerfile
+#echo "COPY ./requirements.txt /home/myapp/requirements.txt" >> Dockerfile
 echo "RUN pip install -r requirements.txt" >> Dockerfile
 #cmd command om python script uit te voeren
 echo "CMD python3 /home/myapp/app.py" >> Dockerfile
